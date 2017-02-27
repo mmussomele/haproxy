88701971a350276ad10deca97e8ad931b31703d6
Kevin Lin <kaikai526@gmail.com>
2017-02-27 16:52:58 -0600

    spec: Create config file at compile time
    Because the haproxy config doesn't depend on any runtime variables, we
    can create it entirely at compile time. This allows us to remove our
    custom image.

--- haproxy.js
+++ haproxy.js
@@ -1,4 +1,5 @@
-var image = "quilt/haproxy";
+var image = "haproxy:1.6.4";
+var configPath = "/usr/local/etc/haproxy/haproxy.cfg";
 
 function Haproxy(n, services, port) {
     services = Array.isArray(services) ? services : [services];
@@ -10,9 +11,13 @@ function Haproxy(n, services, port) {
     var addresses = hostnames.map(function(host) {
       return host + ":" + port;
     });
-    var hapRef = new Container(image).withEnv({
-      "ADDRS": addresses.join(",")
-    });
+    var files = {};
+    files[configPath] = buildConfig(addresses);
+
+
+    var hapRef = new Container(image,
+        ["haproxy-systemd-wrapper", "-p", "/run/haproxy.pid", "-f", configPath]
+    ).withFiles(files);
 
     this.service = new Service("hap", hapRef.replicate(n));
     services.forEach(function(service) {
@@ -27,5 +32,13 @@ function Haproxy(n, services, port) {
         deployment.deploy(this.service);
     };
 };
+
+function buildConfig(addrs) {
+    var config = read("./haproxy.cfg");
+    for (var i = 0 ; i < addrs.length; i++) {
+        config += "\n    server " + i + " " + addrs[i] + " check resolvers dns";
+    }
+    return config;
+}
 
 module.exports.Haproxy = Haproxy;
